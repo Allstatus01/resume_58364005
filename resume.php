0b4c4bb9a76fe727e466bdf6588e21fc6817424c
Kritsanapong Chaitat <43871997+Allstatus01@users.noreply.github.com>
2018-10-13 11:52:22 +0700

    add html file and my picture.
    add resume file (HTML) and my picture.

---
+++ resume.php
@@ -0,0 +1,70 @@
+<html>
+	<body bgcolor="#F4F6F7">
+		<BR>
+		<h1>&nbsp; Kritsanapong Chaitat (58364005)</h1>
+		<h1>&nbsp;---------------------------------------------------------------------------------------------------------------</h1>		
+			&emsp;<img src="pic/tonpalm.jpg" width="350" height="450" 
+			       style="float:left; margin:10px 4px;">
+			<br></br>
+			&emsp;&emsp;<font size="5" color="#D4AC0D"><strong>Full Name:</strong></font>
+			<font size="5">&nbsp;Kritsanapong Chaitat</font><br></br>
+			&emsp;&emsp;<font size="5" color="#D4AC0D"><strong>Nick Name:</strong></font>
+			<font size="5">&nbsp;Tonpalm</font><br></br>
+			&emsp;&emsp;<font size="5" color="#D4AC0D"><strong>Place of Birth:</strong></font>
+			<font size="5">&nbsp;Lamphun, Thailand</font><br></br>
+			&emsp;&emsp;<font size="5" color="#D4AC0D"><strong>Birthday:</strong></font>
+			<font size="5">&nbsp;14th of October 1996</font><br></br>
+			&emsp;&emsp;<font size="5" color="#D4AC0D"><strong>Address :</strong></font>
+			<font size="5">&nbsp;208 Village No.7 Sub-area: Tha Pho, Amphoe Muang, Phitsanulok</font><br></br>
+			&emsp;&emsp;<font size="5" color="#D4AC0D"><strong>Mobile:</strong></font>
+			<font size="5">&nbsp;<a href=#>061-7902939</a></font><br></br>
+			&emsp;&emsp;<font size="5" color="#D4AC0D"><strong>E-mail:</strong></font>
+			<font size="5">&nbsp;<a href=#>kritsanapongc58@email.nu.ac.th</a></font>	<br></br>&emsp;&emsp;&emsp;&nbsp;&nbsp;
+			&emsp;&emsp;&emsp;<font size="5">&nbsp;&nbsp;<a href=#>allstatus01@gmail.com</a></font>	
+			<br></br>
+            <br></br>			
+			<br></br>
+			<h1>&nbsp;---------------------------------------------------------------------------------------------------------------</h1>
+			&emsp;<font size="5"><strong>Education          &nbsp;&nbsp;|</strong></font>
+			<font size="5"><strong>&nbsp; Naresuan University</strong></font><br></br>
+			&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;
+			<font size="5">&nbsp;<strong> Major:</strong> Computer Engineering</font><br></br>
+			&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;
+			<font size="5">&nbsp;<strong> Minor:</strong> Architecture Design</font><br></br>
+			<br></br>
+			&emsp;<font size="5"><strong>Skills &emsp;&emsp;&nbsp;&nbsp;|</strong></font>
+			<font size="5">&nbsp;<strong> Office Skills</strong><br></br>
+			&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;
+			<font size="5">Office and document management, database administration, customer</font><br></br>
+			&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;
+			<font size="5">  support, organization coordination</font><br></br>
+			&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;&nbsp;
+			<font size="5">&nbsp;<strong> Computer Skills</strong><br></br>
+			&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;
+			<font size="5">Microsoft Office(Word, Excel, ect), Adobe Creative Suite</font><br></br>
+			&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;
+			<font size="5">  and Programming(C++, java, etc)</font><br></br>
+			<br></br>
+			&emsp;<font size="5"><strong>Experience                     |</strong></font>
+			<font size="5"><strong>&nbsp; Study CPE in Naresuan University</strong>
+			<font size="4" color="#D0D3D4">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Phitsanulok 2015-2019</font>	
+			<ul><ul><ul><ul><ul>
+				<li>Practice programming skills (Algorithm, DB, ect)<br></br>
+				<li>Learning natural of electric circuit<br></br>
+			</ul></ul></ul></ul></ul>
+			&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;
+			<font size="5"><strong>&nbsp; Apprentice in Lamphun Hospital</strong>
+			<font size="4" color="#D0D3D4">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Lamphun 2018</font>	
+			<ul><ul><ul><ul><ul>
+				<li>Build Program: Reservation Queue<br></br>
+				<li>Participate in Developing Hospital Website
+			</ul></ul></ul></ul></ul>
+			<br></br>
+			&emsp;<font size="5"><strong>Hobbies            &emsp;&nbsp;|</strong></font>
+			<font size="5">&nbsp; Watching Anime, Exercise or Shopping sometime</font>
+			<br></br>
+			&emsp;<font size="5"><strong>Reference          &nbsp;&nbsp;|</strong></font>
+			<font size="5">&nbsp; Available on request</font><br></br>
+		</BR>
+	</body>
+</html>
